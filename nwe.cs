479c00413c62a16342d890b6abaaf1e56b3e5066
haihoangvua0 <167439221+haihoangvua0@users.noreply.github.com>
2024-05-14 16:01:06 +0700

    Add files via upload

---
+++ nwe.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Haihoang
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Nếu bạn nghĩ là ...");
+        }
+    }
+}
